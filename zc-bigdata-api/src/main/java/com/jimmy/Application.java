726af24329c0666d2d87cbd11c79569c05833405
yanyifei <yanyifei@zcckj.com>
2019-04-11 17:10:16 +0800

    说明

--- zc-bigdata-api/src/main/java/com/jimmy/Application.java
+++ zc-bigdata-api/src/main/java/com/jimmy/Application.java
@@ -6,13 +6,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-
 /**
  * Created by Administrator on 2019/4/10/010.
  */
 //@EnableApolloConfig //启动配置中心
 @SpringBootApplication
 @MapperScan("mybatis.mapper")
+//@ComponentScan(basePackages={"com.jimmy"})//spring-boot中扫描包的位置
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
