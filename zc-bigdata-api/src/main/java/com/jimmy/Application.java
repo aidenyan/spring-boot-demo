fa18b4c37c75f481150910beca70c4b3d1523f7a
yanyifei <yanyifei@zcckj.com>
2019-04-15 17:25:56 +0800

    启动事务

--- zc-bigdata-api/src/main/java/com/jimmy/Application.java
+++ zc-bigdata-api/src/main/java/com/jimmy/Application.java
@@ -4,6 +4,7 @@ package com.jimmy;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 //@EnableApolloConfig //启动配置中心
 @SpringBootApplication
+@EnableTransactionManagement//表示启动事务
 //@MapperScan("mybatis.mapper")//与在class上加上@mapper意义一样，扫描对应的mapper.xml
 //@ComponentScan(basePackages={"com.jimmy"})//spring-boot中扫描包的位置.默认的情况下扫描该启动类所在的包
 public class Application {
