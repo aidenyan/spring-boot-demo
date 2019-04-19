43aa9e1804d3dbb3371cda3693281aab041d4687
yanyifei <yanyifei@zcckj.com>
2019-04-19 13:05:32 +0800

    格式

--- zc-bigdata-dao/src/main/java/com/jimmy/mapper/DbMapper.java
+++ zc-bigdata-dao/src/main/java/com/jimmy/mapper/DbMapper.java
@@ -2,9 +2,7 @@ package com.jimmy.mapper;
 
 import com.jimmy.entity.Db;
 import com.jimmy.entity.DbKey;
-import org.apache.ibatis.annotations.Mapper;
 
-@Mapper//与配合使用可以是根据xml进行实例化生成bean
 public interface DbMapper {
     int deleteByPrimaryKey(DbKey key);
 
