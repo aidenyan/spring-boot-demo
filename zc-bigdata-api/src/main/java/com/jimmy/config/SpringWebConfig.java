740f8e22bc21c50dc6a7c587a137c67e6e0d1b2a
yanyifei <yanyifei@zcckj.com>
2019-05-05 14:49:28 +0800

    格式

---
+++ zc-bigdata-api/src/main/java/com/jimmy/config/SpringWebConfig.java
@@ -0,0 +1,19 @@
+package com.jimmy.config;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
+
+/**
+ * Created by Administrator on 2019/5/5/005.
+ */
+@Configuration
+public class SpringWebConfig {
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean(DispatcherServlet dispatcherServlet) {
+        ServletRegistrationBean servletServletRegistrationBean = new ServletRegistrationBean(dispatcherServlet);
+        servletServletRegistrationBean.addUrlMappings("/api/*");
+        return servletServletRegistrationBean;
+    }
+}
